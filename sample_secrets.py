d562cb4ea6381549f0129c1c4ee37ce66b0a1cd0
Don Blair <donblair@gmail.com>
2020-01-15 16:30:02 -0500

    update

---
+++ sample_secrets.py
@@ -0,0 +1,6 @@
+secrets = {
+    "ssid" : "YourWiFiSSID",             # Keep the two '' quotes around the name
+    "password" : "YourWiFiPassword",         # Keep the two '' quotes around password
+    "farmos_pubkey" : "farmosPubKey",
+    "farmos_privkey" : "farmosPrivKey"
+    }
