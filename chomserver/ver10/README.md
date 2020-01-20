9af43a54d62aa14b4cc39077bb60160d150ea170
Don Blair <donblair@gmail.com>
2020-01-19 20:34:07 -0500

    update

---
+++ chomserver/ver10/README.md
@@ -0,0 +1,9 @@
+retains earlier values by re-inserting them
+
+only works with private key 
+(though would be good to give response via html when private key doesn't match)
+
+works with circuitpython
+
+basic:  if lower than threshold, it'll update server
+
---
+++ chomserver/ver10/poster.py
@@ -0,0 +1,31 @@
+import requests 
+import time
+from random import seed
+from random import random
+
+seed(1)
+
+PRIVATE_KEY = 'a123'
+
+#API_ENDPOINT = "http://localhost:8001/api/user"
+API_ENDPOINT = 'http://64.227.0.108:8001/api/user'
+
+
+testvalue = random()
+
+testvalue = 9.
+
+sensorValue='sensorA'
+
+print(testvalue)
+
+data = {'private_key':PRIVATE_KEY, 
+        'sensor':sensorValue, 
+        'value':testvalue,
+        } 
+
+r = requests.post(url = API_ENDPOINT, json = data) 
+
+print('posted')
+
+time.sleep(.1)
