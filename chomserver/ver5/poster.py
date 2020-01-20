3803a57110d67cbb7a8dfdc43a58a506d64d2557
Don Blair <donblair@gmail.com>
2020-01-19 20:40:08 -0500

    update

---
+++ chomserver/ver5/poster.py
@@ -0,0 +1,29 @@
+import requests 
+import time
+from random import seed
+from random import random
+
+seed(1)
+
+PRIVATE_KEY = 'a123'
+
+API_ENDPOINT = "http://localhost:8001/api/user"
+#API_ENDPOINT = 'http://64.227.0.108:8001/api/user'
+
+
+while True:
+
+    testvalue = random()
+
+    print(testvalue)
+
+    data = {'private_key':PRIVATE_KEY, 
+            'sensor':'sensorA', 
+            'value':testvalue,
+            } 
+
+    r = requests.post(url = API_ENDPOINT, json = data) 
+
+    print('posted')
+
+    time.sleep(.1)
