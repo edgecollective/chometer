9459d3a83c4aee063e2319660b884ba2b065fd64
Don Blair <donblair@gmail.com>
2020-01-20 18:21:39 -0500

    update

--- chomserver/ver12/server.js
+++ chomserver/ver12/server.js
@@ -192,10 +192,10 @@ app.post("/api/reading/", (req, res, next) => {
                 return;
             }
             console.log("responding ...")
-            /*res.send({
+            res.send({
                 "message": "success"
             })
-            */
+            
             res.end()
         });
         
