f1f38ec71a970ca17421b22c29e7cdbb59f4c591
Don Blair <donblair@gmail.com>
2020-01-20 12:54:20 -0500

    update

--- chomserver/ver11/spudnik/spud.py
+++ chomserver/ver11/spudnik/spud.py
@@ -7,6 +7,15 @@ import gc
 import time
 import adafruit_hcsr04
 
+SENSOR_ID = 'sensorB'
+APPROACH_THRESHOLD = 50. # cm
+RETREAT_THRESHOLD = 100. # cm
+LAG_PERIOD = 2. # seconds
+
+
+CUSTOMER = 0.  # False
+start_time = 0.
+end_time = 0.
 
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D10, echo_pin=board.D7)
 
@@ -25,14 +34,13 @@ PRIVATE_KEY=secrets['privkey']
 
 print("PK=",PRIVATE_KEY)
 
-#base_url= "http://localhost:8000/api/user"
+#JSON_POST_URL = 'http://64.227.0.108:8001/api/user'
+#JSON_POST_URL = 'http://localhost:8001/api/user'
 
-base_url = "http://192.168.1.254:8000/api/user"
-
-#base_url = "http://httpbin.org/post"
-
-JSON_POST_URL = base_url
-
+#JSON_POST_URL = "http://localhost:8001/api/user"
+#JSON_POST_URL = "http://localhost:8001/api/reading"
+#JSON_POST_URL = "http://192.168.1.254:8001/api/reading"
+JSON_POST_URL = "http://64.227.0.108:8002/api/reading"
 # esp32
 
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
@@ -63,9 +71,36 @@ def connect(essid,password): # note that these are arguments are b'essid' and b'
     # Initialize a requests object with a socket and esp32spi interface
     requests.set_socket(socket, esp)
 
+def post_data(CUSTOMER):
+    print("Posting value:", CUSTOMER)
+    json_data = {'private_key':PRIVATE_KEY, 
+        'sensor':SENSOR_ID, 
+        'value':CUSTOMER
+    } 
+    print("Posting to ",JSON_POST_URL)
+
+    response = requests.post(JSON_POST_URL, json=json_data)
+    print(response.content)
+    response.close()
+    print("posted!")
+
+
+def blink(num_times):
+    for i in range(0,num_times):
+        led.value=True
+        time.sleep(.1)
+        led.value = False
+        time.sleep(.1)
 
 # measure and display loop
-connect(WIFI_ESSID,WIFI_PASS)
+led.value=True
+try:
+    connect(WIFI_ESSID,WIFI_PASS)
+    blink(3)
+except Exception as e:
+    print("error: "+str(e))
+    blink(100)
+
 
 while True:
 
@@ -74,31 +109,23 @@ while True:
     try:
   
         distance = sonar.distance
-        print((distance,))
+        print((distance,),(CUSTOMER,))
 
+        if (distance < APPROACH_THRESHOLD) and (CUSTOMER==False):
+            print ("CUSTOMER APPROACHED!")
+            blink(1)
+            CUSTOMER=1.0
+            post_data(CUSTOMER)
+            time.sleep(LAG_PERIOD)
 
-        json_data = {'private_key':PRIVATE_KEY, 
-            'current':0., 
-            'voltage':0.,
-            'resistance':distance
-        } 
-        #json_data={"distance":distance}
-        print("Posting to ",JSON_POST_URL)
-
-        response = requests.post(JSON_POST_URL, json=json_data)
-        print(response.content)
-        response.close()
-
-        print("Done. Sleeping ... ")
-        led.value=True
-        time.sleep(.05)
-        led.value = False
-        time.sleep(.05)
-
-        time.sleep(.5)
-
+        if (distance > RETREAT_THRESHOLD) and (CUSTOMER==True):
+            print ("CUSTOMER LEFT!")
+            blink(1)
+            CUSTOMER=0.0
+            post_data(CUSTOMER)
+            time.sleep(LAG_PERIOD)
 
     except Exception as e:
 
         print("error: "+str(e))
-        time.sleep(.5)
+        #time.sleep(2)
